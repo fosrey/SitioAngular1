729de69739bfe3c0e49061593eff6945637a9b2a
José Salgado <77949974+fosrey@users.noreply.github.com>
2021-03-23 13:52:18 -0600

    Version 3: CSS

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../Hero'
+
+import {Hero} from '../Hero';
+import {HEROES} from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero={
-    id: 1,
-    name: 'Spiderman',
-    edad: 23,
-    poder: 'Telarañas, Sentido Aracnido',
-    universo: 'Marvel',
-    identidad: 'Pedro Parque'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from "./Hero";
+export const HEROES: Hero[] =[
+
+{id: 11, name: "Superman", edad: 23, poder: "Volar", universo: "xd", identidad: "Nombre12"},
+{id: 12, name: "Batman", edad: 43, poder: "Dinero", universo: "Sanzho", identidad: "Nombre12"},
+{id: 13, name: "Hulk", edad: 23, poder: "Dos", universo: "hgff", identidad: "Nombre12"},
+{id: 14, name: "Spiderman", edad: 34, poder: "Volar", universo: "hf", identidad: "Nombre12"},
+{id: 15, name: "Black Widow", edad: 43, poder: "Volar", universo: "hghgh", identidad: "Nombre12"},
+{id: 16, name: "Bruja Escarlata", edad: 73, poder: "Volar", universo: "xgfd", identidad: "Nombre12"},
+{id: 17, name: "Wonder Woman", edad: 13, poder: "Volar", universo: "gf", identidad: "Nombre12"},
+{id: 18, name: "Flash", edad: 230, poder: "Volar", universo: "fggfg", identidad: "Nombre12"},
+{id: 10, name: "Iron Man", edad: 23, poder: "Volar", universo: "gnb", identidad: "Nombre12"},
+{id: 20, name: "Salcicha", edad: 23, poder: "Volar", universo: "xd", identidad: "Nombre12"}
+];
